d60753debf9011be904094a9f5d5e45e4069dfed
Mathieu LE MAUFF <mathieu@lemauff.pro>
2019-12-20 17:07:24 +0100

    [DEV] Add S905X LibreTech-CC support

---
+++ board/amlogic/s905x-libretech-cc/boot_from_sdcard.cmd
@@ -0,0 +1,10 @@
+mmc dev 0
+setenv fdtaddr 0x01008000
+setenv loadaddr 0x1080000
+setenv devtype "mmc"
+setenv devnum "0:1"
+setenv fdtfile "amlogic/meson-gxl-s905x-libretech-cc.dtb"
+setenv bootargs console=ttyAML0 root=/dev/mmcblk1p2 rootfstype=ext4 rootwait console=ttyAML0,115200 no_console_suspend
+fatload ${devtype} ${devnum} ${fdtaddr} ${fdtfile}
+fatload ${devtype} ${devnum} ${loadaddr} image
+booti ${loadaddr} - ${fdtaddr}
